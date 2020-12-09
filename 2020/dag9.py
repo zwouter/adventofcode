633e7c391144fb7344bee5656b155c44c199daa9
Wouter Suidgeest <w.f.suidgeest@student.utwente.nl>
2020-12-09 11:46:46 +0100

    Extreem inefficiente oneliners

--- 2020/dag9.py
+++ 2020/dag9.py
@@ -1,27 +1,33 @@
+import time
+
+
+# Part 1
 fil = list(map(int, open("data9").readlines()))
-
-i_start = 0
-i_end = preamble = 25
-b = 0
-
-while True:
-    s = fil[i_start:i_end]
-    a = fil[i_end]
-    good = False
-    for x in s:
-        if a - x in s:
-            good = True
-    if not good:
-        b = a
+for i in range(25,len(fil)):
+    s = fil[i-25:i]
+    a = fil[i]
+    if sum(a-x in s for x in s)==0:
         break
-    i_end += 1
-    i_start += 1
-
 print(a)
+
+# Part 2
 for x in range(len(fil)):
     i = x + 2
     while sum(fil[x:i]) < a:
         i += 1
     if sum(fil[x:i]) == a:
         print(min(fil[x:i]) + max(fil[x:i]))
+
+
+# Oneliners
+fil = "data9"
+# Oneliner 1
+print([[f[i]for i in range(25,len(f))if sum(f[i]-x in f[i-25:i]for x in f[i-25:i])<1][0]for f in[list(map(int,open(fil).readlines()))]][0])
+
+starttime = time.time()
+print([sum([sum([min(f[x:i])+max(f[x:i])for i in range(x+2,len(f))if sum(f[x:i])==[f[i]for i in range(25,len(f))if sum(f[i]-x in f[i-25:i]for x in f[i-25:i])<1][0]])for x in range(len(f))])for f in [list(map(int,open(fil).readlines()))]][0])
+
+print(time.time()-starttime)
+
+
 
